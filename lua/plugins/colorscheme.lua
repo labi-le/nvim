6c237518e1b3ed68183f3992caca1f6b91c6f475
labi-le <1a6i1e@gmail.com>
2026-01-30 23:22:21 +0300

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
